cb1b250b3b770ddb99733e3623009e4bb02a6add
John Mark Harrell <hijohnmark@gmail.com>
2024-09-15 12:12:21 +0700

    complete python sequences

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    
+    if length == 0:
+        print([])  # Print an empty list when length is 0
+        return
+    elif length == 1:
+        print([0])  # Print only the first Fibonacci number when length is 1
+        return
+    
+    fibonacci_sequence = [0, 1]
+    
+    # Generate the Fibonacci sequence up to the given length
+    for i in range(2, length):
+        next_value = fibonacci_sequence[i-1] + fibonacci_sequence[i-2]
+        fibonacci_sequence.append(next_value)
+    
+    print(fibonacci_sequence)
